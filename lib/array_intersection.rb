d6bed1cec88a0f8651b24f2f7ba49a99e73aceb1
Danielle Metzner <danimetzner@gmail.com>
2018-10-29 16:32:50 -0700

    Used hash table to solve for intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,35 @@
 # Creates a new array to return the intersection of the two input arrays
+# If m > n
+# Time complexity: O(n)
+# Space complexity: O(m)
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil
+    return []
+  end
+
+  same = []
+
+  if array1.length > array2.length
+    larger = array1
+    shorter = array2
+  else
+    larger = array2
+    shorter = array1
+  end
+
+  hash = Hash.new()
+
+  shorter.each do |num|
+    hash[num] = 1
+  end
+
+  i = 0
+  while i < larger.length
+    if hash[larger[i]] == 1
+      same << larger[i]
+    end
+    i += 1
+  end
+
+  return same
 end
